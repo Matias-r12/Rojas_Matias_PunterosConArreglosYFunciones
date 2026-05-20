62e5851946f3f734c09f87471dc98fe65644d5e7
Matias.Rojas <matias.rojas.suntaxi@udla.edu.ec>
2026-05-20 14:41:36 -0500

    Calculo del promedio

--- PunterosArreglosFunciones.c
+++ PunterosArreglosFunciones.c
@@ -2,12 +2,14 @@
 
 int main()
 {
-    float notas[5]={6.5,7.1,8,6,5};
+    
+    float notas[5]={6.5,7.1,8,6,5},p;
     for (int i = 0; i < 5; i++)
     {
-        printf("\n notas [%.1f] %.1f",notas[i], notas[i]);
+        printf("\n notas [%d] %.1f",i, notas[i]);
     }
-    
+    p=(notas[0]+notas[1]+notas[2]+notas[3]+notas[4])/5.0;
+        printf("\nEl promedio es igual a: %.2f",p);
 
     return 0;
 }
