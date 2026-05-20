8dd4eb8d4e437c9829dabcad59a8f88475297ed4
Matias.Rojas <matias.rojas.suntaxi@udla.edu.ec>
2026-05-20 15:04:49 -0500

    Funcion calcular Promedio*Valor

--- PunterosArreglosFunciones.c
+++ PunterosArreglosFunciones.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+float CalcularP(float notas[5]);
 
-int main()
-{
-    
+int main(){
     float notas[5]={6.5,7.1,8,6,5},p;
-    for (int i = 0; i < 5; i++)
+    float result=CalcularP(notas);
+        printf("\nEl promedio es  a: %.2f",CalcularP(notas));
+    return 0;
+}
+float CalcularP(float notas[5]){
+   float suma=0,p;
+   for (int i = 0; i < 5; i++)
     {
         printf("\n notas [%d] %.1f",i, notas[i]);
-    }
+    } 
     p=(notas[0]+notas[1]+notas[2]+notas[3]+notas[4])/5.0;
-        printf("\nEl promedio es igual a: %.2f",p);
-
-    return 0;
+    return p;
 }
